3aca665c0eb2e3e17f39c145609c29555b6741d1
Matheus Gomes <matheusgomes233231@gmail.com>
2025-06-27 23:07:30 -0300

    Assinatura ajeitada

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let tempo = new Date()
 let horas = tempo.getHours()
+let minutos = tempo.getMinutes()
 let iconeTitulo = document.getElementById("icone-hora-titulo")
+let txtP = document.getElementById("TP")
+txtP.innerText = horas + ":" + minutos
 
 //RELOGIO PARA MUDAR O ICONE DO TITULO DEPENDENDO DO HORARIO
 if (horas >= 7 && horas < 12) {
-    /*alert("Bom dia")*/
     iconeTitulo.href = "assets/dia.png"
 } else if (horas > 12 && horas < 18){
-    /*alert("Boa tarde")*/
     iconeTitulo.href = "assets/tarde.png"
 } else if (horas > 18 || horas < 7){
-    /*alert("Boa noite")*/
     iconeTitulo.href = "assets/noite.png"
 }
 
